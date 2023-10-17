2120e7f10feb3eea185179ad0ffb73e3dd7946fc
Melina Moraiti <145536946+MelinaMoraiti@users.noreply.github.com>
2023-10-17 17:19:01 +0300

    Update profile.js

--- Athletics-eShop/routes/profile.js
+++ Athletics-eShop/routes/profile.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const db = require("../public/scripts/db"); 
+const db = require("../db"); 
 const bcrypt = require('bcrypt');
 
 router.get('/profile', (req, res) => {
